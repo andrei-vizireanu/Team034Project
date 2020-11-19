9b1afd625cf338783e8438446b93168b08ebeb28
vizireanuandrei-cristian <vizireanuandreicristian10@gmail.com>
2020-11-19 14:53:11 +0200

    Test Commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,46 @@
+package com.company;
+import java.sql.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("\nDrivers loaded as properties:");
+        System.out.println(System.getProperty("jdbc.drivers"));
+        System.out.println("\nDrivers loaded by DriverManager:");
+        Enumeration<Driver> list = DriverManager.getDrivers();
+        while (list.hasMoreElements())
+
+            System.out.println(list.nextElement());
+
+        Connection con = null; // a Connection object
+        Statement stmt = null;
+        try {
+            con = DriverManager.getConnection("jdbc:mysql://stusql.dcs.shef.ac.uk/team034", "team034", "4228b661");
+            // use the open connection
+            // for several queries
+
+        }
+        catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        finally{
+            //finally block used to close resources
+            try{
+                if(stmt!=null)
+                    con.close();
+            }catch(SQLException se){
+            }// do nothing
+            try{
+                if(con!=null)
+                    con.close();
+            }catch(SQLException se){
+                se.printStackTrace();
+            }//end finally try
+        }//end try
+        System.out.println("Goodbye, Andrei!");
+        System.out.println("Hi, Ramona");
+
+    }
+}
