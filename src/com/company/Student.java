7aaac983833be5b26443598380e18e81e9e18e24
chigozimmogbolu <chigozimudochi@gmail.com>
2020-11-27 11:24:20 +0000

    chigozim Student class

--- src/com/company/Student.java
+++ src/com/company/Student.java
@@ -1,4 +1,37 @@
 package com.company;
 
-public class Student {
+import javax.swing.*;
+
+public class Student extends JFrame {
+
+    // Needed for serialisation WHAT THIS MEANS?
+    private static final long serialVersionUID = 1L;
+
+    // declaring the variables
+    private static JFrame frame;
+    private final int width = 1000;
+    private final int height = 750;
+
+    public Student(String title){
+
+        //initializing the variables
+        frame = new JFrame(title);
+
+        //center the window
+        MyFrame.centreWindow(frame, width, height);
+
+        //adding the main container to the frame
+        //frame.getContentPane().add(mainContainer);
+        //setting the width and the height of the frame
+        frame.setSize(width,height);
+
+        //don't allow the frame to be resized
+        frame.setResizable(false);
+
+        //making the frame visible
+        frame.setVisible(true);
+
+    }
+
 }
+
