c117821fad06cad88240a542ee12e8bd5d41fed6
chigozimmogbolu <chigozimudochi@gmail.com>
2020-12-03 06:16:08 +0000

    Chigozim commit added a few things to result.java

--- src/com/company/MyResults.java
+++ src/com/company/MyResults.java
@@ -1,6 +1,7 @@
 package com.company;
 
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableModel;
 import java.awt.*;
@@ -32,5 +33,32 @@ public class MyResults {
 
         TableModel tableModel = new DefaultTableModel(rows, columnNames);
         table = new JTable(tableModel);
+
+        viewResults.addActionListener(ae -> {
+
+            if (table.getSelectedRowCount() == 1) {
+
+                Container addGradeContainer = new Container();
+                JFrame dialogFrame = new JFrame();
+                JPanel infoPanel = new JPanel();
+                JPanel buttonsPanel = new JPanel();
+                EmptyBorder borderLabels = new EmptyBorder(15, 0, 0, 0);
+                JScrollPane dialogScroll = new JScrollPane(infoPanel);
+
+                addGradeContainer.setLayout(new BorderLayout());
+                infoPanel.setLayout(new GridLayout(14, 2, 10, 10));
+
+
+                String moduleCode = (String) table.getValueAt(table.getSelectedRow(), 0);
+                String grade = (String) table.getValueAt(table.getSelectedRow(), 1);
+                String pass = (String) table.getValueAt(table.getSelectedRow(), 2);
+                String resit = (String) table.getValueAt(table.getSelectedRow(), 3);
+
+                JLabel gradeLbl = new JLabel("Grade");
+                gradeLbl.setForeground(Color.BLUE);
+                gradeLbl.setBorder(borderLabels);
+            }
+        });
     }
 }
+
